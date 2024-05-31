1bac10c0e85f6b30c95dbf61732a0cc233480706
Gugyoung Kim <kim9young@gmail.com>
2024-05-31 23:11:06 +0900

    SF data

--- To_sim/for_multi.py
+++ To_sim/for_multi.py
@@ -27,5 +27,18 @@ def get_r_sigma_Z(b,theta_random,t,D,omega,N,K,mk,Aij):
     sigma_phi = np.mean(Zs[th:])
     sigma = np.abs(sigma_phi)
     psi = np.angle(sigma_phi)
-    chi = (np.mean(r**2)-sigma**2)*N
+    chi = (r_sm-sigma**2)*N
+    return r_m,sigma,chi
+
+
+def get_r_sigma_Z_MF(b,theta_random,t,D,omega,N,K):
+    th = len(t)//2
+    Zs = RKHG_Z(KU.Kuramoto_mf_AR,theta_random,t,D, args=(omega,N,K,b))
+    r = np.abs(Zs[th:])
+    r_m = np.mean(r)
+    r_sm = np.mean(r**2)
+    sigma_phi = np.mean(Zs[th:])
+    sigma = np.abs(sigma_phi)
+    psi = np.angle(sigma_phi)
+    chi = (r_sm-sigma**2)*N
     return r_m,sigma,chi
